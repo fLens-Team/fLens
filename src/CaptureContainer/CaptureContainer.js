de0ec4f48787b5cbe28c58493a5448a7b6ced137
sakamo-bnn <90294278+sakamo-bnn@users.noreply.github.com>
2024-08-07 23:57:41 +0900

    画面右のフロント終了

--- src/CaptureContainer/CaptureContainer.js
+++ src/CaptureContainer/CaptureContainer.js
@@ -3,36 +3,16 @@ import "./CaptureContainer.css";
 
 // 画像に関するパス
 import CaptureFrame from "../image/CaptureFrame.png";
-// import CaptureBottomLeft from "../image/CaptureBottomLeft.svg";
-// import CaptureBottomRight from "../image/CaptureBottomRight.svg";
-// import DownLoadButton from "../image/DownLoadButton2.svg";
 
-// 画面上全体の領域
+// 画面左部分
 export default function CaptureContainer({ shaderIndex }) {
   return (
     <div className="CaptureContainer">
-      {/* 画面上の中央: カメラのキャプチャ */}
-      {/* <div className="CaptureContainerCenter"> */}
-      {/* キャプチャとフレーム */}
-      {/* <div className="CaptureContainerCenter-top"> */}
-
       {/* キャプチャのフレーム */}
       <img src={CaptureFrame}></img>
 
       {/* キャプチャ */}
       <CameraCapture shaderIndex={shaderIndex} />
-      {/* </div> */}
-
-      {/* キャプチャ下部の装飾とダウンロードボタン */}
-      {/* <div className="CaptureContainerCenter-bottom">
-          <img src={CaptureBottomLeft} className="CaptureContainerCenter-bottom-left"></img>
-          <img src={DownLoadButton} onClick={saveImage} className="CaptureContainerCenter-bottom-center"></img>
-          <img src={CaptureBottomRight} className="CaptureContainerCenter-bottom-right"></img>
-        </div> */}
-      {/* </div> */}
-
-
-
     </div>
   );
 }
